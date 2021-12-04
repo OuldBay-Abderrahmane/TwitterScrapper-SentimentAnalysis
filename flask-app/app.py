f6b1b6b2ed8c8cf26290b5ba8d38504741c3a6af
OuldBay-Abderrahmane <bob@>
2021-12-04 20:17:07 +0100

    Web app fix

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,33 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import Flask, render_template, request, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, DateField, SubmitField
-from wtforms.validators import DataRequired
-import os
-
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-
-
-app = Flask(__name__)
-app.from_object(Config)
+from wtforms.validators import Optional
+from flask_bootstrap import Bootstrap
 
 
 class RequestForm(FlaskForm):
-    words = StringField('Username', validators=[DataRequired()])
-    exacts = PasswordField('Password')
-    from_date = DateField('Date')
-    until_date = DateField('Date')
-    submit = SubmitField('Sign In')
+    words = StringField('Words', validators=[Optional()])
+    exacts = StringField('Exacts', validators=[Optional()])
+    from_date = DateField('From', validators=[Optional()])
+    until_date = DateField('Until', validators=[Optional()])
+    submit = SubmitField('Scrape!')
 
 
-@app.route("/")
-@app.route("/index")
-def hello_world():
-    return render_template("base.html")
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'C2HWGVoMGfNTBsrYQg8EcMrdTimkZfAb'
+Bootstrap(app)
 
 
-@app.route('/request')
-def request():
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/request', methods=['GET', 'POST'])
+def form():
     form = RequestForm()
-    return render_template('requests.html', title='Request', form=form)
+    if form.validate_on_submit():
+        dict_form = {"words": f'{form.words.data}', "exacts": f'{form.exacts.data}',
+                     "from_date": f'{form.from_date.data}', "until_date": f'{form.until_date.data}'}
+        print(dict_form)
+        return redirect('/results')
+    return render_template('request.html', form=form)
+
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
+
+
+if __name__ == '__main__':
+    app.run()
